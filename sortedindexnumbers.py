51f9ce5608c0b1d94b4a003b451190f93eef7127
saichandrapandraju <41769919+saichandrapandraju@users.noreply.github.com>
2018-08-16 13:04:52 +0530

    Create sortedindexnumbers.py

---
+++ sortedindexnumbers.py
@@ -0,0 +1,14 @@
+n=int(raw_input())
+new=[]
+list=[int(x) for x in raw_input().split()]
+for i in range(0,n):
+	if(i==list[i]):
+		new.append(i)
+		#print(new)
+	elif((new==[]) and (i==(n-1))):
+		print('-1')
+s=sorted(new)
+k=len(s)
+for i in range(0,k):
+	print s[i],
+		
